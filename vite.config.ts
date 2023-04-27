087c5a0096de5eda8bf711a2a3f2272763d2447c
Itallo <dornelaszxd@gmail.com>
2023-04-27 20:16:52 -0300

    fix: vite config updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 const config = {
@@ -8,10 +9,57 @@ const config = {
       name: '@saibweb/saibweb-components',
     },
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'saibweb-components.ts',
+  },
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        use: 'raw-loader',
+        include: [path.resolve(__dirname, './src', 'components')],
+        options: {
+          presets: [['react-app', { flow: false, typescript: true }]],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        exclude: [
+          path.resolve(__dirname, '../', 'client'),
+          path.resolve(__dirname, 'node_modules', 'cas-mt4-reader-renderer'),
+          path.resolve(__dirname, 'node_modules', 'cas-mdoc-renderer'),
+        ],
+      },
+      {
+        test: /((.module).)*css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+              localsConvention: 'camelCase',
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+        ],
+        include: [
+          path.resolve(__dirname, '../', 'client'),
+          path.resolve(__dirname, 'node_modules', 'cas-mt4-reader-renderer'),
+          path.resolve(__dirname, 'node_modules', 'cas-mdoc-renderer'),
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css'],
   },
 };
 
