9fc8a1d728cff918c46a7e3774dcbbd6921b0199
Itallo <dornelaszxd@gmail.com>
2023-04-27 14:52:03 -0300

    feat: structur for publis

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const config = {
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'saibweb-components',
+    },
+  },
+};
+
+export default defineConfig(config);
