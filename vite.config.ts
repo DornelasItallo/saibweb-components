2abee02a7d463afc927393ee8708acf9f1cb10eb
Itallo <dornelaszxd@gmail.com>
2023-04-27 20:32:11 -0300

    fix: update loader from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
 const config = {
@@ -9,57 +8,17 @@ const config = {
       name: '@saibweb/saibweb-components',
     },
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'saibweb-components.ts',
-  },
   plugins: [react()],
   module: {
     rules: [
       {
-        test: /\.(j|t)sx?$/,
-        use: 'raw-loader',
-        include: [path.resolve(__dirname, './src', 'components')],
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
         options: {
-          presets: [['react-app', { flow: false, typescript: true }]],
+          target: 'es2015',
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-        exclude: [
-          path.resolve(__dirname, '../', 'client'),
-          path.resolve(__dirname, 'node_modules', 'cas-mt4-reader-renderer'),
-          path.resolve(__dirname, 'node_modules', 'cas-mdoc-renderer'),
-        ],
-      },
-      {
-        test: /((.module).)*css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                localIdentName: '[path][name]__[local]--[hash:base64:5]',
-              },
-              localsConvention: 'camelCase',
-              sourceMap: true,
-            },
-          },
-          'postcss-loader',
-        ],
-        include: [
-          path.resolve(__dirname, '../', 'client'),
-          path.resolve(__dirname, 'node_modules', 'cas-mt4-reader-renderer'),
-          path.resolve(__dirname, 'node_modules', 'cas-mdoc-renderer'),
-        ],
-      },
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css'],
   },
 };
 
