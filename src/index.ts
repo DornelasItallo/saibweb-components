7b9fffb36f6cba1aa416e0e239f773961040a7c9
Itallo <dornelaszxd@gmail.com>
2023-04-27 23:09:03 -0300

    fix: add ts to dist build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import '../src/styles/globals.css';
-import { SaibWebModal } from './components/SaibWebModal';
+import { SaibWebModal } from './components/SaibWebModal/';
 
 export { SaibWebModal };
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
+import typescript from '@rollup/plugin-typescript';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 const config = {
   build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
     lib: {
-      entry: './src',
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@saibweb/saibweb-components',
     },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
+    },
   },
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
 };
 
 export default defineConfig(config);
