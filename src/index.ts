a0a92caeccba8681254aa0c55ae82aa76f9e032f
Itallo <dornelaszxd@gmail.com>
2023-04-27 22:19:20 -0300

    fix: resolve imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import '../src/styles/globals.css';
-import { SaibWebModal } from './components/SaibWebModal';
+import { SaibWebModal } from './components/SaibWebModal/index.tsx';
 
 export { SaibWebModal };
