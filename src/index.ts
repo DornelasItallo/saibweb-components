ac4e61f779744f04d7bc2a6f6555ed69d731fdd0
Itallo <dornelaszxd@gmail.com>
2023-04-27 22:01:58 -0300

    fix: remove components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import '../src/styles/globals.css';
-import { SaibWebButton } from './components/SaibWebButton';
-import { SaibWebHeader } from './components/SaibWebHeader';
-import { SaibWebInput } from './components/SaibWebInput';
 import { SaibWebModal } from './components/SaibWebModal';
 
-export { SaibWebButton, SaibWebHeader, SaibWebInput, SaibWebModal };
+export { SaibWebModal };
