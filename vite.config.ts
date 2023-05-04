ed50d30991f9ad49872ffc465a5e22ca7a62565f
Itallo <dornelaszxd@gmail.com>
2023-05-04 16:19:27 -0300

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,19 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite';
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
 
 
 const config = {
   plugins: [
-    dts({
-      include: ['src/'],
-      insertTypesEntry: true,
-    }),
+    dts(),
     react(),
     tsConfigPaths(),
   ],
   build: {
-    manifest: true,
-    minify: true,
     reportCompressedSize: true,
     lib: {
       entry: './src/index',
       name: '@saibweb/saibweb-components',
-    },
-    rollupOptions: {
-      plugins: [
-        peerDepsExternal(),
-        // typescript({ useTsconfigDeclarationDir: true }),
-        resolve(),
-        commonjs(),
-        postcss({
-          extensions: ['.css']
-        })
-      ],
     },
   },
 };
